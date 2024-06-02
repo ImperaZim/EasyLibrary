256b3a180ef7475004fb4d396c1c75a626b61ce9
impera <imperaartz1@gmail.com>
2024-06-02 18:01:58 -0300

    (...): General updates and fixes that do not require a specific push.

--- examples/PluginExample/src/ImperaZim/PluginExample.php
+++ examples/PluginExample/src/ImperaZim/PluginExample.php
@@ -43,7 +43,7 @@ final class PluginExample extends PluginToolkit {
     );
 
     Query::getServerInfo('166.0.189.217', 19132)->onCompletion(
-      function (array $data): void {
+      function(array $data) {
         var_dump($data);
       }
     );
--- src/library/network/Query.php
+++ src/library/network/Query.php
@@ -9,6 +9,7 @@ use function json_encode;
 use pocketmine\utils\Internet;
 use pocketmine\promise\Promise;
 use pocketmine\promise\PromiseResolver;
+use pocketmine\utils\InternetRequestResult;
 
 /**
 * Class Query
@@ -30,26 +31,23 @@ final class Query {
     ]);
 
     $resolver = new PromiseResolver();
+    $error = null;
 
-    Internet::postURL(
-      $url, 
-      $postData, 
-      10, 
-      [], 
-      function (string $response) use ($resolver): void {
-      $data = json_decode($response, true);
+    $response = Internet::postURL($url, $postData, 10, [], $error);
+
+    if ($error !== null) {
+      $resolver->reject(['error' => $error]);
+    } elseif ($response instanceof InternetRequestResult) {
+      $data = json_decode($response->getBody(), true);
       if (json_last_error() !== JSON_ERROR_NONE) {
-        $resolver->reject([]);
-        return;
+        $resolver->reject(['error' => 'Invalid JSON response']);
+      } else {
+        $resolver->resolve($data);
       }
-      $resolver->resolve($data);
-    },
-      function () use ($resolver): void {
-        $resolver->reject([]);
-      }
-     );
+    } else {
+      $resolver->reject(['error' => 'Unknown error']);
+    }
 
     return $resolver->getPromise();
   }
-
 }
