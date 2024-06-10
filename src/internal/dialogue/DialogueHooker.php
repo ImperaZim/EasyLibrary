e1743ecaebb2c0f9477f18c51a28c25ef1c6d9fa
impera <imperaartz1@gmail.com>
2024-06-10 03:00:28 -0300

    (...): General updates and fixes that do not require a specific push.

--- src/internal/dialogue/DialogueHooker.php
+++ src/internal/dialogue/DialogueHooker.php
@@ -4,11 +4,16 @@ declare(strict_types = 1);
 
 namespace internal\dialogue;
 
+use Generator;
 use BadMethodCallException;
 use pocketmine\player\Player;
+use SOFe\AwaitGenerator\Await;
 use internal\dialogue\Dialogue;
 use pocketmine\plugin\PluginBase;
+use internal\dialogue\types\AsyncDialogue;
 use internal\dialogue\player\PlayerManager;
+use internal\dialogue\exception\DialogueException;
+use internal\dialogue\textures\DefaultDialogueTexture;
 
 /**
 * Class DialogueHooker
@@ -43,7 +48,7 @@ final class DialogueHooker {
   * @throws BadMethodCallException If the dialogue is already registered.
   */
 	public static function register(?PluginBase $plugin) : void {
-		self::$manager === null || throw new BadMethodCallException("NpcDialog is already registered");
+		self::$manager === null || throw new BadMethodCallException("Dialog is already registered");
 		self::$manager = new PlayerManager();
 		self::$manager->init($plugin);
 	}
@@ -55,7 +60,7 @@ final class DialogueHooker {
   * @param bool $update_existing
   */
 	public static function send(Player $player, Dialogue $dialogue, bool $update_existing = false) : void{
-		self::$manager !== null || throw new BadMethodCallException("NpcDialog is not registered");
+		self::$manager !== null || throw new BadMethodCallException("Dialog is not registered");
 		self::$manager->getPlayer($player)->sendDialogue($dialogue, $update_existing);
 	}
 	
@@ -64,27 +69,27 @@ final class DialogueHooker {
  * @param Player $player The player requesting the dialogue interaction.
  * @param string $name The name of the dialogue.
  * @param string $text The text displayed in the dialogue.
- * @param NpcDialogueTexture|null $texture The texture of the NPC.
+ * @param DialogueTexture|null $texture The texture of the NPC.
  * @param array $buttons The buttons available in the dialogue.
  * @param array|null $button_mapping Optional mapping of button indices to their corresponding actions.
  * @param bool $update_existing Whether to update an existing dialogue if one is already active.
  * @return Generator A generator that yields the result of the dialogue request.
  */
-public static function request(Player $player, string $name, string $text, ?NpcDialogueTexture $texture = null, array $buttons = [], ?array $button_mapping = null, bool $update_existing = false) : Generator {
-    self::$manager !== null || throw new BadMethodCallException("NpcDialog is not registered");
-    $instance = self::$manager->getPlayerNullable($player) ?? throw new NpcDialogueException("Player is not connected", NpcDialogueException::ERR_PLAYER_DISCONNECTED);
-    $texture ??= new DefaultNpcDialogueTexture(DefaultNpcDialogueTexture::TEXTURE_NPC_10);
+public static function request(Player $player, string $name, string $text, ?DialogueTexture $texture = null, array $buttons = [], ?array $button_mapping = null, bool $update_existing = false) : Generator {
+    self::$manager !== null || throw new BadMethodCallException("Dialog is not registered");
+    $instance = self::$manager->getPlayerNullable($player) ?? throw new DialogueException("Player is not connected", DialogueException::ERR_PLAYER_DISCONNECTED);
+    $texture ??= new DefaultDialogueTexture(DefaultDialogueTexture::TEXTURE__10);
     $button_mapping ??= array_keys($buttons);
-    return yield from Await::promise(static fn(Closure $resolve, Closure $reject) => $instance->sendDialogue(new AsyncNpcDialogue($name, $text, $texture, array_values($buttons), $button_mapping, $resolve, $reject), $update_existing));
+    return yield from Await::promise(static fn(Closure $resolve, Closure $reject) => $instance->sendDialogue(new AsyncDialogue($name, $text, $texture, array_values($buttons), $button_mapping, $resolve, $reject), $update_existing));
 }
 
 /**
  * Removes the current NPC dialogue for a player.
  * @param Player $player The player whose NPC dialogue should be removed.
- * @return NpcDialogue|null The removed NPC dialogue, or null if none was removed.
+ * @return Dialogue|null The removed NPC dialogue, or null if none was removed.
  */
-public static function remove(Player $player) : ?NpcDialogue {
-    self::$manager !== null || throw new BadMethodCallException("NpcDialog is not registered");
+public static function remove(Player $player) : ?Dialogue {
+    self::$manager !== null || throw new BadMethodCallException("Dialog is not registered");
     return self::$manager->getPlayerNullable($player)?->removeCurrentDialogue()?->dialogue;
 }
 
