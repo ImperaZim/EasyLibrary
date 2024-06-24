abf6a7d365f9c8e91794a1cff9f2e329e02540d5
impera <imperaartz1@gmail.com>
2024-06-24 10:44:08 -0300

    (...): General updates and fixes that do not require a specific push.

--- src/library/item/ItemFactory.php
+++ src/library/item/ItemFactory.php
@@ -7,27 +7,46 @@ namespace library\item;
 use library\item\exception\ItemException;
 
 use pocketmine\item\Item;
+use pocketmine\block\Block;
+use pocketmine\utils\Utils;
 use pocketmine\nbt\TreeRoot;
+use pocketmine\item\ItemTypeIds;
+use pocketmine\block\BlockTypeIds;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\scheduler\AsyncTask;
 use pocketmine\scheduler\AsyncPool;
+use Symfony\Component\Filesystem\Path;
 use pocketmine\item\StringToItemParser;
+use pocketmine\utils\AssumptionFailedError;
 use pocketmine\nbt\LittleEndianNbtSerializer;
+use pocketmine\data\bedrock\BedrockDataFiles;
 use pocketmine\data\SavedDataLoadingException;
 use pocketmine\data\runtime\RuntimeDataWriter;
+use pocketmine\data\bedrock\item\ItemTypeNames;
 use pocketmine\data\bedrock\item\SavedItemData;
+use pocketmine\network\mcpe\convert\TypeConverter;
 use pocketmine\world\format\io\GlobalItemDataHandlers;
 use pocketmine\world\format\io\GlobalBlockStateHandlers;
+use pocketmine\network\mcpe\protocol\types\ItemTypeEntry;
+use pocketmine\network\mcpe\convert\ItemTypeDictionaryFromDataHelper;
+use function file_get_contents;
+use function json_decode;
+use function str_replace;
+use function strtolower;
 
 /**
 * Class ItemFactory
 * @package library\item
 */
 final class ItemFactory {
-
+  
   /** @var Item[] */
-  private static array $registeredItems = [];
+	private static array $registeredItems = [];
+	/** @var Block[] */
+	private static array $registeredBlocks = [];
 
+	private int $nextBlockId = BlockTypeIds::FIRST_UNUSED_BLOCK_ID + 1;
+  
   /**
   * Initializes the ItemFactory by registering item handlers in an asynchronous pool.
   * @param AsyncPool $asyncPool The asynchronous pool to use for registering item handlers.
@@ -35,27 +54,96 @@ final class ItemFactory {
   */
   public static function init(AsyncPool $asyncPool): void {
     /**
-     * TODO: empty
-     */
+    * TODO: empty
+    */
   }
 
   /**
-  * Register a new item.
-  * @param Item $item
-  * @return string|null
+  * @param Item          $item the Item to register
+  * @param int           $runtimeId the runtime id that will be used by the server to send the item to the player.
+  * This usually can be found using BDS, or included in {@link \pocketmine\BEDROCK_DATA_PATH/required_item_list.json}. for custom items, you should generate this manually.
+  * @param bool          $force
+  * @param string        $namespace the item's namespace. This usually can be found in {@link ItemTypeNames}.
+  * @param \Closure|null $serializeCallback the callback that will be used to serialize the item.
+  * @param \Closure|null $deserializeCallback the callback that will be used to deserialize the item.
+  *
+  * @return void
+  * @see ItemTypeDictionaryFromDataHelper
+  * @see libItemRegistrar::getRuntimeIdByName()
   */
-  public static function register(Item $item): ?string {
-    try {
-      $name = strtolower(str_replace(' ', '_', $item->getVanillaName()));
-      self::$registeredItems[$name] = $item;
-      GlobalItemDataHandlers::getDeserializer()->map($item->getTypeId(), fn() => clone $item);
-      GlobalItemDataHandlers::getSerializer()->map($item, fn() => new SavedItemData($item->getTypeId()));
-      StringToItemParser::getInstance()->register($name, fn() => clone $item);
-      return $name;
-    } catch (ItemException $e) {
-      new \crashdump($e);
-      return null;
+  public static function registerItem(Item $item, int $runtimeId, bool $force = false, string $namespace = "", ?\Closure $serializeCallback = null, ?\Closure $deserializeCallback = null) : void {
+    if ($serializeCallback !== null) {
+      /** @phpstan-ignore-next-line */
+      Utils::validateCallableSignature(static function(Item $item) : SavedItemData {}, $serializeCallback);
     }
+    if ($deserializeCallback !== null) {
+      Utils::validateCallableSignature(static function(SavedItemData $data) : Item {}, $deserializeCallback);
+    }
+    if (isset(self::$registeredItems[$item->getTypeId()]) && !$force) {
+      throw new AssumptionFailedError("Item {$item->getTypeId()} is already registered");
+    }
+    self::$registeredItems[$item->getTypeId()] = $item;
+
+    StringToItemParser::getInstance()->override($item->getName(), static fn() => clone $item);
+    $serializer = GlobalItemDataHandlers::getSerializer();
+    $deserializer = GlobalItemDataHandlers::getDeserializer();
+
+    $namespace = $namespace === "" ? "minecraft:" . strtolower(str_replace(" ", "_", $item->getName())) : $namespace;
+
+    (function() use ($item, $serializeCallback, $namespace) : void {
+      $this->itemSerializers[$item->getTypeId()] = $serializeCallback !== null ? $serializeCallback : static fn() => new SavedItemData($namespace);
+    })->call($serializer);
+    (function() use ($item, $deserializeCallback, $namespace) : void {
+      if (isset($this->deserializers[$item->getName()])) {
+        unset($this->deserializers[$item->getName()]);
+      }
+      $this->map($namespace, $deserializeCallback !== null ? $deserializeCallback : static fn(SavedItemData $_) => clone $item);
+    })->call($deserializer);
+
+    $dictionary = TypeConverter::getInstance()->getItemTypeDictionary();
+    (function() use ($item, $runtimeId, $namespace) : void {
+      $this->stringToIntMap[$namespace] = $runtimeId;
+      $this->intToStringIdMap[$runtimeId] = $namespace;
+      $this->itemTypes[] = new ItemTypeEntry($namespace, $runtimeId, true);
+    })->call($dictionary);
+  }
+
+  /**
+  * Returns a next item id and increases it.
+  *
+  * @return int
+  * @deprecated Use {@link ItemTypeIds::newId()} instead.
+  */
+  public function getNextItemId() : int {
+    return ItemTypeIds::newId();
+  }
+
+  public function getItemByTypeId(int $typeId) : ?Item {
+    return self::$registeredItems[$typeId] ?? null;
+  }
+
+  /**
+  * Returns the runtime id of given item name. (only for vanilla items)
+  *
+  * @param string $name
+  *
+  * @return int|null null if runtime id does not exist.
+  */
+  public static function getRuntimeIdByName(string $name) : ?int {
+    static $mappedJson = [];
+    if ($mappedJson === []) {
+      $mappedJson = self::$reprocessKeys(json_decode(file_get_contents(Path::join(BedrockDataFiles::REQUIRED_ITEM_LIST_JSON)), true));
+    }
+    $name = str_replace(" ", "_", strtolower($name));
+    return $mappedJson[$name]["runtime_id"] ?? null;
+  }
+
+  private static function reprocessKeys(array $data) : array {
+    $new = [];
+    foreach ($data as $key => $value) {
+      $new[str_replace("minecraft:", "", $key)] = $value;
+    }
+    return $new;
   }
 
   /**
