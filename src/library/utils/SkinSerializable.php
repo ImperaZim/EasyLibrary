8aaa8d843920952b3445e4833a9e2434e8c19b68
impera <imperaartz1@gmail.com>
2024-07-10 22:59:21 -0300

    (...): General updates and fixes that do not require a specific push.

--- src/library/utils/SkinSerializable.php
+++ src/library/utils/SkinSerializable.php
@@ -13,24 +13,26 @@ use pocketmine\entity\Skin;
 final class SkinSerializable {
 
   /**
+  * Ensure data is UTF-8 encoded.
+  * @param string $data The data to ensure encoding.
+  * @return string The UTF-8 encoded data.
+  */
+  private static function ensureUtf8(string $data): string {
+    return mb_convert_encoding($data, 'UTF-8', 'UTF-8');
+  }
+
+  /**
   * Serializes the Skin object into a JSON string.
   * @param Skin $skin The skin containing the data to serialize.
   * @return string The JSON string containing the serialized data of the Skin.
   */
   public static function jsonSerialize(Skin $skin): string {
-    var_dump([
-      'skinId' => $skin->getSkinId(),
-      'skinData' => $skin->getSkinData(),
-      'capeData' => $skin->getCapeData(),
-      'geometryName' => $skin->getGeometryName(),
-      'geometryData' => $skin->getGeometryData(),
-    ]);
     return json_encode([
-      'skinId' => $skin->getSkinId(),
-      'skinData' => $skin->getSkinData(),
-      'capeData' => $skin->getCapeData(),
-      'geometryName' => $skin->getGeometryName(),
-      'geometryData' => $skin->getGeometryData(),
+      'skinId' => self::ensureUtf8($skin->getSkinId()),
+      'skinData' => self::ensureUtf8($skin->getSkinData()),
+      'capeData' => self::ensureUtf8($skin->getCapeData()),
+      'geometryName' => self::ensureUtf8($skin->getGeometryName()),
+      'geometryData' => self::ensureUtf8($skin->getGeometryData()),
     ], JSON_THROW_ON_ERROR);
   }
 
