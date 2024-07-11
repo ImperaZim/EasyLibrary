a4b2835eba5a723ab0676e2896121ccd11052251
impera <imperaartz1@gmail.com>
2024-07-10 21:56:01 -0300

    (...): General updates and fixes that do not require a specific push.

--- src/library/utils/SkinSerializable.php
+++ src/library/utils/SkinSerializable.php
@@ -13,9 +13,9 @@ use pocketmine\entity\Skin;
 final class SkinSerializable {
 
   /**
-  * Serializes the Skin object into an associative skin.
-  * @param Skin $skin The skin containing the data to deserialize.
-  * @return string The string containing the serialized data of the Skin.
+  * Serializes the Skin object into a JSON string.
+  * @param Skin $skin The skin containing the data to serialize.
+  * @return string The JSON string containing the serialized data of the Skin.
   */
   public static function jsonSerialize(Skin $skin): string {
     return json_encode([
@@ -24,22 +24,22 @@ final class SkinSerializable {
       'capeData' => $skin->getCapeData(),
       'geometryName' => $skin->getGeometryName(),
       'geometryData' => $skin->getGeometryData(),
-    ]);
+    ], JSON_THROW_ON_ERROR);
   }
 
   /**
-  * Deserializes data from an associative array to create a new instance of Skin.
-  * @param string $data The array containing the data to deserialize.
+  * Deserializes data from a JSON string to create a new instance of Skin.
+  * @param string $data The JSON string containing the data to deserialize.
   * @return Skin The new instance of Skin created based on the provided data.
   */
   public static function jsonDeserialize(string $data): Skin {
-    $data = json_decode($data);
+    $decodedData = json_decode($data, true, 512, JSON_THROW_ON_ERROR);
     return new Skin(
-      $data['skinId'],
-      $data['skinData'],
-      $data['capeData'] ?? "",
-      $data['geometryName'] ?? "",
-      $data['geometryData'] ?? ""
+      $decodedData['skinId'],
+      $decodedData['skinData'],
+      $decodedData['capeData'] ?? "",
+      $decodedData['geometryName'] ?? "",
+      $decodedData['geometryData'] ?? ""
     );
   }
 }
