b48be95d56d3e1c0e02550241c80c95533ce5255
impera <imperaartz1@gmail.com>
2024-07-14 15:57:26 -0300

    (...): General updates and fixes that do not require a specific push.

--- src/internal/customitem/CustomItemManager.php
+++ src/internal/customitem/CustomItemManager.php
@@ -89,7 +89,6 @@ final class CustomItemManager {
 
   public function registerDefaultItems(array $data) : void {
     foreach ($data as $name => $itemData) {
-      var_dump(self::getItem($name, $itemData)->getVanillaName());
       $this->registerItem(self::getItem($name, $itemData));
     }
     $this->refresh();
