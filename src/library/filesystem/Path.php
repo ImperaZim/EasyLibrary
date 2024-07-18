f78b4405c68c1ae625d6f03034a4e485425d96e9
impera <imperaartz1@gmail.com>
2024-07-18 17:14:37 -0300

    (...): General updates and fixes that do not require a specific push.

--- src/library/filesystem/Path.php
+++ src/library/filesystem/Path.php
@@ -223,6 +223,7 @@ final class Path {
             'content' => File::deserializeContent($file->getExtension(), file_get_contents($file->getRealPath()))
           ];
         }
+        var_dump($file);
       }
     } catch (FileSystemException $e) {
       new \crashdump($e);
