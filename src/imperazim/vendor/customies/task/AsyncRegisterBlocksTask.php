bb7840b33b6ddfc473bd22557a76b8d6faddc7c5
impera <imperaartz1@gmail.com>
2024-09-23 18:23:52 -0300

    Block Fix Test (Custom)

--- src/imperazim/vendor/customies/task/AsyncRegisterBlocksTask.php
+++ src/imperazim/vendor/customies/task/AsyncRegisterBlocksTask.php
@@ -12,16 +12,20 @@ use pocketmine\scheduler\AsyncTask;
 
 final class AsyncRegisterBlocksTask extends AsyncTask {
 
-	private array $classNames = [];
-	private array $serializer = [];
-	private array $deserializer = [];
+	private ThreadSafeArray $classNames;
+	private ThreadSafeArray $serializer;
+	private ThreadSafeArray $deserializer;
 
 	/**
 	 * @param Closure[] $blocks
 	 * @phpstan-param array<string, array{(Closure(int): Block), (Closure(BlockStateWriter): Block), (Closure(Block): BlockStateReader)}> $blocks
 	 */
 	public function __construct(private string $cachePath, array $blockFuncs) {
-		foreach($blockFuncs as $identifier => [$block, $serializer, $deserializer, $className]){
+	  $this->classNames = new ThreadSafeArray();
+		$this->serializer = new ThreadSafeArray();
+		$this->deserializer = new ThreadSafeArray();
+		
+		foreach($blockFuncs as $identifier => [$className, $serializer, $deserializer]){
 			$this->classNames[$identifier] = $className;
 			$this->serializer[$identifier] = $serializer;
 			$this->deserializer[$identifier] = $deserializer;
